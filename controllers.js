2f85da265e07a816599f1881c0fe6a86f3b4a2a2
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-05-06 14:06:24 +0330

    addfiledb

--- controllers.js
+++ controllers.js
@@ -1,24 +1,30 @@
 const BlogSchema = require('./model')
+const Blog = require('./model')
 
 const deleteFacultyMember =async (req,res)=>{
    try {
-    
+    const {id:_id}= req.params
+    const newBlog = await Blog.findByIdAndDelete(_id);
+    res.statuse(202).json(newBlog)
    } catch (error) {
-    
+    res.statuse(500).json({message:error})
    }
 
 }
 const addFacultyMember =async (req,res)=>{
     try {
-     
+     const userData = req.body
+     const newD = Blog.creat(userData)
+     res.statuse(200).json()
     } catch (error) {
-     
+     res.statuse(500).json({message:error})
     }
  
  }
  const getAllFacultyMembers =async (req,res)=>{
     try {
-     
+     const users = await Blog.find({})
+     res.statuse(200).json(users)
     } catch (error) {
      
     }
