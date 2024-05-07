71e60419603e96cbb7eaeff36c843676e2d31149
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-05-07 11:58:26 +0330

    get

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const db = require('./conectdb')
 const Blog = require('./model')
 // 
+app.use(express.urlencoded({ extended: true }))
 const cors = require('cors')
 const bodyParser =require('body-parser')
 const router = require("express").Router()
@@ -12,6 +13,8 @@ app.use(cors())
 app.use(express.json())
 const port =5000
 db()
+
+
 app.listen(port,()=>{
     console.log('your port is run');
 })
